2c517c4cb6b8ff7eb4ee4772b24525036b650863
Owen <384232819@qq.com>
2018-04-30 13:22:34 +0800

    用OkHttp访问网络数据用Gson解析JSON格式数据.

--- app/src/main/java/org/crazyit/myshop/fragment/HomeFragment.java
+++ app/src/main/java/org/crazyit/myshop/fragment/HomeFragment.java
@@ -17,14 +17,27 @@ import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.crazyit.myshop.R;
 import org.crazyit.myshop.adapter.DividerItemDecortion;
 import org.crazyit.myshop.adapter.HomeCategoryAdapter;
+import org.crazyit.myshop.bean.Banner;
 import org.crazyit.myshop.bean.HomeCategory;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by Administrator on 2018/4/27.
@@ -40,6 +53,10 @@ public class HomeFragment extends Fragment {
 
     private RecyclerView  mRecyclerView;
     private HomeCategoryAdapter mAdatper;
+
+    private Gson mGson=new Gson();
+
+    private List<Banner> mBanner;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -48,12 +65,53 @@ public class HomeFragment extends Fragment {
 
         indicator=view.findViewById( R.id.custom_indicator);
 
-        initSlider();
-
-
-        //
+        requestImages();
+//        initSlider();
         initRecyclerView(view);
         return view;
+    }
+
+    private  void requestImages(){
+        String url="http://112.124.22.238:8081/course_api/banner/query?type=1";
+        OkHttpClient client=new OkHttpClient();
+
+
+        RequestBody body=new FormBody.Builder()
+                .add("type","1")
+                .build();
+
+        Request request=new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+
+             client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    //判断是否返回数据成功了
+                    if (response.isSuccessful()){
+                        //如果成功了,那么我们便从这里面取出数据
+                        String json=response.body().string();
+//                        Log.i(TAG,"json="+json);
+                        //尖括号中传入我们需要转换的数据类型
+                        Type type=new TypeToken<List<Banner>>(){}.getType();
+                        mBanner=mGson.fromJson(json,type);
+                        //接下来我们需要将Banner数据放到initSlider()方法中
+                        //首先我们需要先改造一下initSlider()这个方法
+
+                        initSlider();
+
+                    }
+                }
+            });
+
+
     }
 
     private void initRecyclerView( View view) {
@@ -87,58 +145,73 @@ public class HomeFragment extends Fragment {
     }
 
     private  void initSlider(){
-        TextSliderView textSliderView = new TextSliderView(this.getActivity());
-        textSliderView.image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t2416/102/20949846/13425/a3027ebc/55e6d1b9Ne6fd6d8f.jpg");
-        textSliderView.description("新品推荐");
-        textSliderView.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
-            @Override
-            public void onSliderClick(BaseSliderView baseSliderView) {
 
-                Toast.makeText(HomeFragment.this.getActivity(),"新品推荐", Toast.LENGTH_LONG).show();
 
+        if (mBanner!=null){
+            for (Banner banner:mBanner){
+
+                TextSliderView textSliderView=new TextSliderView(this.getActivity());
+                 textSliderView.image(banner.getImgUrl());
+                 textSliderView.description(banner.getName());
+                textSliderView.setScaleType(BaseSliderView.ScaleType.Fit);
+                mSliderLayout.addSlider(textSliderView);
             }
-        });
+        }
+//        TextSliderView textSliderView = new TextSliderView(this.getActivity());
+//        textSliderView.image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t2416/102/20949846/13425/a3027ebc/55e6d1b9Ne6fd6d8f.jpg");
+//        textSliderView.description("新品推荐");
+//        textSliderView.setScaleType(BaseSliderView.ScaleType.Fit);
+//        textSliderView.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+//            @Override
+//            public void onSliderClick(BaseSliderView baseSliderView) {
+//
+//                Toast.makeText(HomeFragment.this.getActivity(),"新品推荐", Toast.LENGTH_LONG).show();
+//
+//            }
+//        });
+//
+//
+//
+//        TextSliderView textSliderView2 = new TextSliderView(this.getActivity());
+//        textSliderView2.image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t1507/64/486775407/55927/d72d78cb/558d2fbaNb3c2f349.jpg");
+//        textSliderView2.description("时尚男装");
+//        textSliderView.setScaleType(BaseSliderView.ScaleType.Fit);
+//
+//        textSliderView2.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+//            @Override
+//            public void onSliderClick(BaseSliderView baseSliderView) {
+//
+//                Toast.makeText(HomeFragment.this.getActivity(),"时尚男装",Toast.LENGTH_LONG).show();
+//
+//            }
+//        });
+//
+//
+//
+//        TextSliderView textSliderView3 = new TextSliderView(this.getActivity());
+//        textSliderView3.image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t1363/77/1381395719/60705/ce91ad5c/55dd271aN49efd216.jpg");
+//        textSliderView3.description("家电秒杀");
+//        textSliderView.setScaleType(BaseSliderView.ScaleType.Fit);
+//
+//
+//        textSliderView3.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+//            @Override
+//            public void onSliderClick(BaseSliderView baseSliderView) {
+//
+//                Toast.makeText(HomeFragment.this.getActivity(),"家电秒杀",Toast.LENGTH_LONG).show();
+//
+//            }
+//        });
 
 
 
-        TextSliderView textSliderView2 = new TextSliderView(this.getActivity());
-        textSliderView2.image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t1507/64/486775407/55927/d72d78cb/558d2fbaNb3c2f349.jpg");
-        textSliderView2.description("时尚男装");
+//        mSliderLayout.addSlider(textSliderView);
+//        mSliderLayout.addSlider(textSliderView2);
+//        mSliderLayout.addSlider(textSliderView3);
 
-        textSliderView2.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
-            @Override
-            public void onSliderClick(BaseSliderView baseSliderView) {
+        mSliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
 
-                Toast.makeText(HomeFragment.this.getActivity(),"时尚男装",Toast.LENGTH_LONG).show();
-
-            }
-        });
-
-
-
-        TextSliderView textSliderView3 = new TextSliderView(this.getActivity());
-        textSliderView3.image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t1363/77/1381395719/60705/ce91ad5c/55dd271aN49efd216.jpg");
-        textSliderView3.description("家电秒杀");
-
-
-        textSliderView3.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
-            @Override
-            public void onSliderClick(BaseSliderView baseSliderView) {
-
-                Toast.makeText(HomeFragment.this.getActivity(),"家电秒杀",Toast.LENGTH_LONG).show();
-
-            }
-        });
-
-
-
-        mSliderLayout.addSlider(textSliderView);
-        mSliderLayout.addSlider(textSliderView2);
-        mSliderLayout.addSlider(textSliderView3);
-
-     //   mSliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
-
-        mSliderLayout.setCustomIndicator(indicator);
+//        mSliderLayout.setCustomIndicator(indicator);
         mSliderLayout.setCustomAnimation(new DescriptionAnimation());
         mSliderLayout.setPresetTransformer(SliderLayout.Transformer.RotateUp);
         mSliderLayout.setDuration(3000);
